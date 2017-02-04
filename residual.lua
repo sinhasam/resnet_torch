f598c2a3162c71a983fe2019b9d800cd77e68668
Samarth Sinha <samarth.infofull@gmail.com>
2017-02-04 02:38:26 -0500

    finished making model, added a few nonlinearities and linearities outside of the model in the paper. might add spatial dropouts

--- residual.lua
+++ residual.lua
@@ -3,6 +3,7 @@ require 'torch'
 -- require 'cudnn'
 -- require 'cunn'
 
+
 local SpatialConvolution = nn.SpatialConvolution
 local AvgPool = nn.SpatialAveragePooling
 local ReLU = nn.ReLU
@@ -10,35 +11,60 @@ local BatchNorm = nn.SpatialBatchNormalization
 local MaxPool = nn.SpatialMaxPooling
 
 
+local numClasses = 8
+
+
 local function firstBlock()
 	local model = nn.Sequential()
 	model:add(SpatialConvolution(3, 64, 7, 7, 2, 2, 3, 3)) -- 3 by 3 pad
 	model:add(BatchNorm(64))
-	model:add(ReLU())
+	model:add(ReLU(true))
 	model:add(MaxPool(3, 3, 2, 2))
 end
 
 
-local function basicBlock(dim)
+local function basicBlock(inputDim, outputDim, stride)
 	local new = nn.Sequential()
-	new:add(SpatialConvolution(3, dim, 3, 3, 1, 1)) -- no pad
+	new:add(SpatialConvolution(inputDim, outputDim, 3, 3, stride, stride, 1, 1)) -- no pad
 	new:add(BatchNorm(dim))
-	new:add(ReLU())
-	new:add(SpatialConvolution(dim, dim, 3, 3, 1, 1))
+	new:add(ReLU(true))
+	new:add(SpatialConvolution(outputDim, outputDim, 3, 3, 1, 1, 1, 1))
 	new:add(BatchNorm(dim))
 	
 	local shortCut = nn.Sequential()
 	shortCut:add(nn.ConcatTable():add(new):add(nn.Identity())) -- need to test if this way of adding residue works
 	shortCut:add(nn.CAddTable())
-	shortCut:add(ReLU)
+	shortCut:add(ReLU(true))
 	return shortCut
 end
 
-local model = nn.Sequential()
-model:add(firstBlock())
 
-for i=1, 4 do
-	model:add(basicBlock(64))
+local function makeModel() -- 18 layer model
+	local model = nn.Sequential()
+
+	model:add(firstBlock()) -- 224*224 -> 112*112
+	model:add(basicBlock(3, 64, 1))
+	
+	model:add(basicBlock(64, 64, 1))
+	model:add(basicBlock(64, 128, 2)) -- 56*56
+
+	model:add(basicBlock(128, 128, 1))
+	model:add(basicBlock(128, 256, 2)) -- 28*28
+
+	model:add(basicBlock(256, 256, 1))
+	model:add(basicBlock(256, 512, 2)) -- 14*14
+
+	model:add(basicBlock(512, 512, 1))
+	model:add(basicBlock(512, 512, 2)) -- 7*7
+
+	model:add(AvgPool(7, 7, 1, 1))
+	model:add(nn.View(512))
+	model:add(nn.Linear(512, 120))
+	model:add(ReLU(true))
+	model:add(nn.Linear(120, 84))
+	model:add(nn.ReLU(true))
+	model:add(84, 10)
+	model:add(nn.LogSoftMax())
+	return model
 end
 
--- model:
